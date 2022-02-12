0e6224a4774de2f9531739825d4bc44c65b2e3ee
Joel Weller <97563644+jweller55@users.noreply.github.com>
2022-02-12 10:42:36 -0700

    Line of sight puzzle

--- Main/js/plugins.js
+++ Main/js/plugins.js
@@ -8,5 +8,6 @@ var $plugins =
 {"name":"YEP_SaveEventLocations","status":true,"description":"v1.06 Enable specified maps to memorize the locations of\nevents when leaving and loading them upon reentering map.","parameters":{}},
 {"name":"DMY_SkipVideo","status":true,"description":"Skip video on key press","parameters":{"Disabler switch":"0"}},
 {"name":"AS_InputSettings","status":true,"description":"WASD Movement, Disable F keys and Disable mouse/touch input.","parameters":{"WASD Control":"true","Disable F2 F3 and F4":"false","Disable Gamepad":"false","Disable Mouse":"false","Disable Mouse Move":"false"}},
-{"name":"TerraxLighting","status":true,"description":"v1.5.1 Creates an extra layer that darkens a map and adds lightsources!","parameters":{"Player radius":"200","Add to options":"Yes","Option menu entry":"Lighting effects","Reset Lights":"No","Save DaynightHours":"0","Save DaynightMinutes":"0","Save DaynightSeconds":"0","Flashlight offset":"0","Screensize X":"866","Screensize Y":"630","Kill Switch":"No"}}
+{"name":"TerraxLighting","status":true,"description":"v1.5.1 Creates an extra layer that darkens a map and adds lightsources!","parameters":{"Player radius":"200","Add to options":"Yes","Option menu entry":"Lighting effects","Reset Lights":"No","Save DaynightHours":"0","Save DaynightMinutes":"0","Save DaynightSeconds":"0","Flashlight offset":"0","Screensize X":"866","Screensize Y":"630","Kill Switch":"No"}},
+{"name":"lineofsight","status":true,"description":"","parameters":{}}
 ];
